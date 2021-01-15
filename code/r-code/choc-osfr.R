9e2bed119b1a69db1be96c245f74a7145268ddcd
Sam Harper <sbh4th@gmail.com>
2021-01-15 16:23:06 -0500

    add draft of git-to-OSF workflow

--- code/r-code/choc-osfr.R
+++ code/r-code/choc-osfr.R
@@ -1,6 +1,22 @@
+#  program:  choc-osfr.R
+#  task:     write from github to OSF repository
+#  input:    git repo (various files)
+#  output:   none
+#  project:  chocolate and happiness
+#  author:   sam harper \ 2021-01-15
+
 # load packages
+library(git2r)
 library(osfr)
 library(tidyverse)
+
+## Create a temporary directory to hold the repository
+path <- file.path(tempfile(pattern="git2r-"), "git2r")
+dir.create(path, recursive=TRUE)
+
+## Clone the git2r repository
+repo <- clone("https://github.com/sbh4th/choc-happy", path)
+summary(repo)
 
 proj <- osf_retrieve_node("3fcxd")
 proj
@@ -8,4 +24,4 @@ osf_ls_nodes(proj)
 
 df <- osf_retrieve_node("ev38k")
 df
-osf_ls_files()
+osf_ls_files(df)
